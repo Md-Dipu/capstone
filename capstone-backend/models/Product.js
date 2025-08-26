aefb17c872c37ddcd2de75b331540ba4bbaab77a
mushfikaraisa <mushfikaraisa@gmail.com>
2025-08-27 01:00:14 +0600

    feat(models): create product model

--- capstone-backend/models/Product.js
+++ capstone-backend/models/Product.js
@@ -1,6 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({}, {timestamps: true});
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true, min: 0 },
+    description: { type: String },
+    imageUrl: { type: String, required: true },
+    category: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
