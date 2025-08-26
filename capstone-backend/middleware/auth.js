b30f90adffb1176ba37463275fd57b7e8d4efb87
mushfikaraisa <mushfikaraisa@gmail.com>
2025-08-27 01:00:54 +0600

    feat(auth): add support for role-based access control

--- capstone-backend/middleware/auth.js
+++ capstone-backend/middleware/auth.js
@@ -1,16 +1,23 @@
 const { verifyAccessToken } = require("../utils/jwt");
 
-exports.protect = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  if (!authHeader?.startsWith("Bearer "))
-    return res.status(401).json({ message: "Unauthorized" });
+exports.auth = (...roles) => {
+  return (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader?.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
-  const token = authHeader.split(" ")[1];
-  try {
-    const decoded = verifyAccessToken(token);
-    req.user = decoded;
-    next();
-  } catch {
-    return res.status(403).json({ message: "Invalid token" });
-  }
+    const token = authHeader.split(" ")[1];
+    try {
+      const decoded = verifyAccessToken(token);
+      req.user = decoded;
+
+      if (roles.length > 0 && !roles.includes(decoded.role)) {
+        return res.status(403).json({ message: "Forbidden" });
+      }
+      next();
+    } catch {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+  };
 };
