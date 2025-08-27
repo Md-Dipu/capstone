dc8fbd7629969b90eff04d33d217c21b195418d0
mushfikaraisa <mushfikaraisa@gmail.com>
2025-08-27 20:30:26 +0600

    feat(models): add Order model

---
+++ capstone-backend/models/Order.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    payment: {
+      type: String,
+      enum: ["pending", "completed", "failed"],
+      default: "pending",
+    },
+    status: {
+      type: String,
+      enum: ["processing", "shipped", "delivered", "cancelled"],
+      default: "processing",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
